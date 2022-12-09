f58edd073a3d2dffab4fb65b82c5a4e3737973fd
Fe195 <114395573+Fe195@users.noreply.github.com>
2022-12-09 23:40:31 +0530

    Update App.js

--- App.js
+++ App.js
@@ -19,21 +19,3 @@ const moveFocusToPreviousInput = (eventOriginationInputNumber) => {
   if (eventOriginationInputNumber === 0) {
     return;
   }
-
-  inputs[eventOriginationInputNumber - 1].focus();
-};
-
-const onInputChange = (event) => {
-  const inputNumber = parseInt(event.target.getAttribute("data-number"));
-
-  if (event.key === "Backspace") {
-    moveFocusToPreviousInput(inputNumber);
-    return;
-  }
-
-  moveFocusToNextInput(inputNumber);
-};
-
-inputs.forEach((input) => {
-  input.addEventListener("keyup", onInputChange);
-});
